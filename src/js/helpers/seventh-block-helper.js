98e2e060584be8fed84b88ce0edafa8238ab72ff
Dotsen Ivan <dotsenvania@gmail.com>
2024-01-19 15:27:23 +0200

    Added the eighth and ninth section and fixed bugs

--- src/js/helpers/seventh-block-helper.js
+++ src/js/helpers/seventh-block-helper.js
@@ -19,37 +19,39 @@ window.addEventListener('resize', moveBlockOnResize);
 const screens = document.querySelectorAll('.screen');
 const menuInfo = document.querySelectorAll('.more_item');
 
-function menuActive(elem) {
-    if(!elem) {
-        menuInfo.forEach((item, i) =>
-        i == 0 ? (item.style.color = 'white') : (item.style.color = null)
-      ); 
-      return; 
-    }
-  menuInfo.forEach((i) =>
-    elem === i ? (i.style.color = 'white') : (i.style.color = null)
+function menuActive(num) {
+  if (!num) {
+    menuInfo.forEach((item, i) =>
+      i+1 === 1 ? (item.style.color = 'white') : (item.style.color = null)
+    );
+    return;
+  }
+  menuInfo.forEach((item) =>
+    num === +item.getAttribute('data-by-screen-id') ? (item.style.color = 'white') : (item.style.color = null)
   );
 }
 
-function activeScreen(screen) {
-    if(!screen) {
-        screens.forEach((item, i) =>
-        i == 0 ? (item.style.opacity = '1') : (item.style.opacity = '0')
-      ); 
-      return; 
-    }
-    screens.forEach((i) =>
-    screen === i ? (i.style.opacity = '1') : (i.style.opacity = '0')
-  );
+function activeScreen(numScreen) {
+  if (!numScreen) {
+    screens.forEach((item, i) =>
+    i+1 === 1 ? (item.style.opacity = '1') : (item.style.opacity = '0')
+    );
+    return;
+  }
+  screens.forEach((item) => {
+    numScreen === +item.getAttribute('data-screen-id')
+      ? (item.style.opacity = '1')
+      : (item.style.opacity = '0');
+  });
 }
 
-
-menuActive(); 
-activeScreen()
+let activNum = 2
+menuActive(activNum);
+activeScreen(activNum);
 
 menuInfo.forEach((i, num) => {
   i.addEventListener('click', (e) => {
-    menuActive(i);
-    activeScreen(screens[num]); 
+    menuActive(+i.getAttribute('data-by-screen-id'));
+    activeScreen(+i.getAttribute('data-by-screen-id'));
   });
 });
